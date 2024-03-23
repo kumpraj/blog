4a1ea4920a0ed5367ecf6192cadb1461ef181fa5
Prajwal Kumar <kumpraj@gmail.com>
2024-03-23 22:32:52 +0530

    added Dockerfile to all services

--- comments/index.js
+++ comments/index.js
@@ -19,7 +19,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 
     const { content } = req.body;
     const comment = commentsByPostId[req.params.id] || [];
-    comment.push({id: commentId, content});
+    comment.push({id: commentId, content, status: 'pending'});
 
     commentsByPostId[req.params.id] = comment;
 
@@ -28,7 +28,8 @@ app.post('/posts/:id/comments', async (req, res) => {
         data: {
             id: commentId,
             content,
-            postId: req.params.id
+            postId: req.params.id,
+            status: 'pending'
         }
     })
 
@@ -36,8 +37,30 @@ app.post('/posts/:id/comments', async (req, res) => {
 
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log('Received Event', req.body.type);
+
+    const { type, data } = req.body;
+
+    if(type === 'CommentModerated') {
+        const { id, postId, status, content } = data;
+        const comments = commentsByPostId[postId];
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+        comment.status = status;
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentUpdated',
+            data: {
+               id,
+               postId,
+               status,
+               content 
+            }
+        })
+    }
+
     res.send({});
 })
 
